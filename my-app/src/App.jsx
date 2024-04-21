86d3c459989f48e8b94f6fc30313f36c12f878e0
baimbrams <baimz.bramz@gmail.com>
2024-04-21 21:03:35 +0700

    "feature Benefit"

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,5 @@
 import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
+import Benefit from "./components/Benefit.jsx";
 
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
@@ -8,6 +9,7 @@ const App = () => {
       <div className="pt-[76px] lg:pt-[5.5rem] overflow-hidden">
         <Header />
         <Hero />
+        <Benefit />
       </div>
       <ButtonGradient />
     </>
